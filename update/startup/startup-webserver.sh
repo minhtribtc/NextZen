0b41cc4ee691b0a5008784c3b6396ece39a95302
minhtribtc <144902346+minhtribtc@users.noreply.github.com>
2024-01-07 19:54:48 +0700

    Add files via upload

--- update/startup/startup-webserver.sh
+++ update/startup/startup-webserver.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# UPDATE VHOST
+
 
 #Update Nextcloud
 clpctl vhost-template:delete --name='NextZen-Nextcloud'
@@ -8,41 +10,74 @@ clpctl vhost-template:add --name='NextZen-Nextcloud' --file=https://raw.githubus
 clpctl vhost-template:delete --name='NextZen-Jellyfin'
 clpctl vhost-template:add --name='NextZen-Jellyfin' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-Jellyfin
 
-#Update OpenWRT
+# 1 Update OpenWRT 
 clpctl vhost-template:delete --name='NextZen-OpenWRT'
 clpctl vhost-template:add --name='NextZen-OpenWRT' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-OpenWRT
 
-#Update Proxmox
+# 2 Update Proxmox
 clpctl vhost-template:delete --name='NextZen-Proxmox'
 clpctl vhost-template:add --name='NextZen-Proxmox' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-Proxmox
 
-#Update AdguardHome
+# 3 Update Web clp
+clpctl vhost-template:delete --name='NextZen-Web'
+clpctl vhost-template:add --name='NextZen-Web' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-Web
+
+# 4 Update AdguardHome
 clpctl vhost-template:delete --name='NextZen-AdguardHome'
 clpctl vhost-template:add --name='NextZen-AdguardHome' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-AdguardHome
 
-#Update Portainer
+# 5 Update Portainer
 clpctl vhost-template:delete --name='NextZen-Portainer'
 clpctl vhost-template:add --name='NextZen-Portainer' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-Portainer
 
-#Update NAS
+# 6 Update Webfile
+clpctl vhost-template:delete --name='NextZen-Webfile'
+clpctl vhost-template:add --name='NextZen-Webfile' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-Webfile
+
+# 7 Update Dashboard-Admin
+clpctl vhost-template:delete --name='NextZen-Admin'
+clpctl vhost-template:add --name='NextZen-Admin' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-Admin
+
+# 8 Update NAS
 clpctl vhost-template:delete --name='NextZen-NAS'
 clpctl vhost-template:add --name='NextZen-NAS' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-NAS
 
-#Update Wireguard
+# 9 Update NASfile
+clpctl vhost-template:delete --name='NextZen-NASfile'
+clpctl vhost-template:add --name='NextZen-NASfile' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-NASfile
+
+# 10 Update NCAIO
+clpctl vhost-template:delete --name='NextZen-NCAIO'
+clpctl vhost-template:add --name='NextZen-NCAIO' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-NCAIO
+
+# 11 Update Jellyfin
+clpctl vhost-template:delete --name='NextZen-Jellyfin'
+clpctl vhost-template:add --name='NextZen-Jellyfin' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-Jellyfin
+
+# 12 Update Wireguard
 clpctl vhost-template:delete --name='NextZen-Wireguard'
 clpctl vhost-template:add --name='NextZen-Wireguard' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-Wireguard
 
-#Update HomeAssistant
+# 13 Update HomeAssistant
 clpctl vhost-template:delete --name='NextZen-HomeAssistant'
 clpctl vhost-template:add --name='NextZen-HomeAssistant' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-HomeAssistant
 
-#Update CameraNVR
+# 14 Update Proxmoxfile
+clpctl vhost-template:delete --name='NextZen-Proxmoxfile'
+clpctl vhost-template:add --name='NextZen-Proxmoxfile' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-Proxmoxfile
+
+# 15 Update CameraNVR
 clpctl vhost-template:delete --name='NextZen-CameraNVR'
 clpctl vhost-template:add --name='NextZen-CameraNVR' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-CameraNVR
 
-#Update CameraNVR
+#Update Vaultwarden
 clpctl vhost-template:delete --name='NextZen-Vaultwarden'
 clpctl vhost-template:add --name='NextZen-Vaultwarden' --file=https://raw.githubusercontent.com/minhtribtc/NextZen/main/update/vhost/NextZen-Vaultwarden
+
+
+# UPDATE LOCAL WEB URL
+
+
 
 
 rm startup-webserver.sh
