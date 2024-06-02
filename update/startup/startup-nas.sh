e62590851503df188d19a08e724c7fbff9114157
minhtribtc <144902346+minhtribtc@users.noreply.github.com>
2024-06-03 01:38:56 +0700

    Update startup-nas.sh

--- update/startup/startup-nas.sh
+++ update/startup/startup-nas.sh
@@ -24,36 +24,11 @@ else
 fi
 #delay 30s
 sleep 30
-# Kiểm tra xem thư mục /DATA có tồn tại hay không
-if [ -d "$DATA" ]; then
-    echo "Thư mục /DATA đã tồn tại. Không thực hiện thêm bước nào."
-else
-    # Nếu thư mục /DATA không tồn tại, tắt NextZenOS
-    sudo systemctl stop casaos*.service
-    echo "Đã tắt NextZenOS vui lòng chọn vùng lưu trữ mặc định"
-fi
-
-# kiểm tra portainer_agent có chạy hay không
-if docker inspect --format '{{.State.Running}}' portainer_agent 2>/dev/null | grep -q "true"; then
-    echo "portainer_agent đang chạy. Không cần thực hiện thêm bước nào."
-else
-    echo "portainer_agent không đang chạy. Đang xoá container và khởi động lại docker..."
-    docker rm portainer_agent 2>/dev/null || echo "Không thể xoá portainer_agent. Có thể container không tồn tại."
-    systemctl restart docker
-    docker run -d \
-      -p 9001:9001 \
-      --name portainer_agent \
-      --restart=always \
-      -v /var/run/docker.sock:/var/run/docker.sock \
-      -v /var/lib/docker/volumes:/var/lib/docker/volumes \
-      portainer/agent:2.19.2
-    
-fi
 
 
 #update nextcloud ip
 # Đường dẫn đến tệp cần kiểm tra
-FILE="/DATA/AppData/Docker/volumes/nextcloud_aio_nextcloud/_data/config/config.php"
+FILE="/NextZEN/App/System/volumes/nextcloud_aio_nextcloud/_data/config/config.php"
 
 # Chuỗi đầu tiên để kiểm tra
 LOCAL_GATEWAY="0 => '127.0.0.1'"
