bec64b0f32aa90f0ef9f5c3ce919178ba0946947
minhtribtc <144902346+minhtribtc@users.noreply.github.com>
2024-06-03 01:46:40 +0700

    Update startup-nas.sh

--- update/startup/startup-nas.sh
+++ update/startup/startup-nas.sh
@@ -14,18 +14,6 @@ nextzen=/NextZen
 sleep 30
 
 
-# Kiểm tra xem thư mục mntDATA có tồn tại hay không
-if [ -d "$mntDATA" ]; then
-    echo "Thư mục mntDATA đã tồn tại. Không thực hiện thêm bước nào."
-else
-    # Nếu thư mục mntDATA không tồn tại, tạo liên kết đến thư mục DATA trong thư mục mnt
-    ln -s /DATA /mnt/DATA
-    echo "Đã tạo liên kết đến thư mục DATA trong thư mục mnt với tên là DATA."
-fi
-#delay 30s
-sleep 30
-
-
 #update nextcloud ip
 # Đường dẫn đến tệp cần kiểm tra
 FILE="/NextZEN/App/System/volumes/nextcloud_aio_nextcloud/_data/config/config.php"
